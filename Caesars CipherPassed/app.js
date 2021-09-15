2e4b217de6371e4bf0066aaeb76e2af16c34f902
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-09-15 19:20:46 +0200

    project 3 commit

---
+++ Caesars CipherPassed/app.js
@@ -0,0 +1,19 @@
+// My Way
+
+function rot13(str) {
+  let newStr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (!/[^a-zA-Z]/.test(str[i])) {
+      let code = str.charCodeAt(i) + 13;
+      if (code > 90) {
+        code = 65 + 13 - (91 - str.charCodeAt(i));
+      }
+      newStr.push(String.fromCharCode(code));
+    } else {
+      newStr.push(str[i]);
+    }
+  }
+  return newStr.join('');
+}
+
+console.log(rot13('SERR CVMMN!'));
