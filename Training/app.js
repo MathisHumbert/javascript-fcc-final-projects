dd32c424151dd208c3dac56950cf90397d3e7774
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-09-17 12:35:56 +0200

    foudnArr working

--- Training/app.js
+++ Training/app.js
@@ -1,21 +1,24 @@
 function checkCashRegister(price, cash, cid) {
   let moneyBack = cash - price;
   let change = null;
+  let finalArr = [];
 
-  console.log(getTheMoney(moneyBack, change, cid));
+  console.log(`final change: ${getTheMoney(moneyBack, change, cid, finalArr)}`);
   return change;
 }
 
-function getTheMoney(moneyBack, change, cid) {
+function getTheMoney(moneyBack, change, cid, arr) {
   // case 100
   if (moneyBack >= 100 && cid[8][1] !== 0) {
     change += 100;
     moneyBack -= 100;
+    arr.push([cid[8][0], cid[8][1]]);
   }
   // case 20
   else if (moneyBack >= 20 && cid[7][1] !== 0) {
     change += 20;
     moneyBack -= 20;
+    arr.push([cid[7][0], cid[7][1]]);
   }
   // case 10
   else if (moneyBack >= 10 && cid[6][1] !== 0) {
@@ -53,8 +56,7 @@ function getTheMoney(moneyBack, change, cid) {
     moneyBack -= 0.01;
   }
 
-  console.log(change);
-  console.log(moneyBack.toFixed(2));
+  console.log(arr);
 
   // if the change is correct
   if (moneyBack === 0) {
@@ -62,7 +64,7 @@ function getTheMoney(moneyBack, change, cid) {
   }
   // else run the func again
   else {
-    return getTheMoney(moneyBack.toFixed(2), change, cid);
+    return getTheMoney(moneyBack.toFixed(2), change, cid, arr);
   }
 }
 
@@ -77,3 +79,19 @@ checkCashRegister(3.26, 100, [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100],
 ]);
+
+let arr1 = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100],
+];
+
+function foundArr(arr, arr2) {
+  return arr.some((arr) => arr[0] == arr2[0]);
+}
